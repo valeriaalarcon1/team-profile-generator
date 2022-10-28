5a5aaaa7a9895fa3f97adc20b3e20aa34ca018e4
Valeria <valeriaalarcon@Valerias-MacBook-Pro.local>
2022-10-28 16:34:13 -0400

    Altered index.js code

--- index.js
+++ index.js
@@ -9,144 +9,163 @@ const Manager = require('./lib/Manager');
 const team = [];
 
 // prompt inputs
-function managerPrompt() {
-   inquirer
-        .prompt([
-            {
-                type: 'input',
-                message: 'Please enter the name of the team manager.',
-                name: 'name',
-            },
-            {
-                type: 'input',
-                message: 'Please enter your ID.',
-                name: 'ID',
-            },
-            {
-                type: 'input',
-                message: 'Please enter your email.',
-                name: 'email',
-            },
-            {
-                type: 'input',
-                message: 'Please enter your office number.',
-                name: 'officeNumber',
-            }
-        ])
+async function managerPrompt() {
+    return new Promise((resolve, reject) => {
+        inquirer
+                .prompt([
+                    {
+                        type: 'input',
+                        message: 'Please enter the name of the team manager.',
+                        name: 'name',
+                    },
+                    {
+                        type: 'input',
+                        message: 'Please enter your ID.',
+                        name: 'ID',
+                    },
+                    {
+                        type: 'input',
+                        message: 'Please enter your email.',
+                        name: 'email',
+                    },
+                    {
+                        type: 'input',
+                        message: 'Please enter your office number.',
+                        name: 'officeNumber',
+                    }
+                ])
 
-    .then((managerData) => {
-        const {name, ID, email, officeNumber} = managerData;
-        const manager = new Manager (name, ID, email, officeNumber)
-        team.push(manager);
-    });
+        .then((managerData) => {
+            const {name, ID, email, officeNumber} = managerData;
+            const manager = new Manager (name, ID, email, officeNumber)
+            team.push(manager);
+        })
+        
+        .then(() => {
+            resolve();
+        })
+    })
 }
 
-function employeePrompt() {
-    inquirer
-        .prompt([
-            {
-                type: 'list',
-                message: 'Please select the role of the employee you would like to add.',
-                name: 'role',
-                choices: ["Engineer", "Intern"],
-            },
-            {
-                type: 'input',
-                message: 'Please enter the name of the employee.',
-                name: 'name',
-            },
-            {
-                type: 'input',
-                message: 'Please enter their ID.',
-                name: 'ID',
-            },
-            {
-                type: 'input',
-                message: 'Please enter their email.',
-                name: 'email',
-            },
-            {
-                type: 'input',
-                message: 'Please enter their github username.',
-                name: 'github',
-                when: (input) => input.role === "Engineer",
-            },
-            {
-                type: 'input',
-                message: 'Please enter their school.',
-                name: 'school',
-                when: (input) => input.role === "Intern",
-            },
-            {
-                type: 'list',
-                message: 'Would you like to add another member to the team?',
-                name: 'addMore',
-                choices: ["Yes", "No"]
-            },
-        ])
+async function employeePrompt() {
+    return new Promise((resolve, reject) => {
+        inquirer
+            .prompt([
+                {
+                    type: 'list',
+                    message: 'Please select the role of the employee you would like to add.',
+                    name: 'role',
+                    choices: ["Engineer", "Intern"],
+                },
+                {
+                    type: 'input',
+                    message: 'Please enter the name of the employee.',
+                    name: 'name',
+                },
+                {
+                    type: 'input',
+                    message: 'Please enter their ID.',
+                    name: 'ID',
+                },
+                {
+                    type: 'input',
+                    message: 'Please enter their email.',
+                    name: 'email',
+                },
+                {
+                    type: 'input',
+                    message: 'Please enter their github username.',
+                    name: 'github',
+                    when: (input) => input.role === "Engineer",
+                },
+                {
+                    type: 'input',
+                    message: 'Please enter their school.',
+                    name: 'school',
+                    when: (input) => input.role === "Intern",
+                },
+                {
+                    type: 'list',
+                    message: 'Would you like to add another member to the team?',
+                    name: 'addMore',
+                    choices: ["Yes", "No"]
+                },
+            ])
 
-    .then((employeeData) => {
-        const {name, ID, email, github, school} = employeeData;
-        if (role === "Engineer") {
-            employee = new Engineer (name, ID, email, github);
-        } else if (role === "Intern") {
-            employee = new Intern (name, ID, email, school);
-        }
-        team.push(employee);
-        if (addMore === "Yes") {
-            return employeeInfo(team);
-        } else {
-            return team;
-        }
+        .then((employeeData) => {
+            console.log(employeeData);
+            const {name, role, ID, email, github, school, addMore} = employeeData;
+            if (role === "Engineer") {
+                employee = new Engineer (name, ID, email, github);
+            } else if (role === "Intern") {
+                employee = new Intern (name, ID, email, school);
+            }
+            team.push(employee);
+            if (addMore === "Yes") {
+                return employeePrompt(team);
+            } else {
+                return team;
+            }
+        })
+        .then(() => {
+            resolve();
+        })
     });
 }
 
 
 const createCard = () => {
-    team.forEach((person) => {
-        if (person.role === "Manager") {
+    const teamCards = team.map((person) => {
+        console.log(person);
+        if (person.getRole() === "Manager") {
             return `
             <div class="card" style="width: 18rem;">
             <div class="card-body">
-                <h4 class="card-title">${person.name}</h4>
-                <h6 class="card-text">${person.role}</h6>
+                <h4 class="card-title">${person.getName()}</h4>
+                <h6 class="card-text">${person.getRole()}</h6>
                 <ul class="list-group list-group-flush">
-                    <li class="list-group-item">ID: ${person.ID}</li>
-                    <li class="list-group-item">Email: ${person.email}</li>
+                    <li class="list-group-item">ID: ${person.getID()}</li>
+                    <li class="list-group-item">Email: ${person.getEmail()}</li>
                     <li class="list-group-item">Office number: ${person.officeNumber}</li>
                 </ul>
             </div>
         `
-        } else if (person.role === "Engineer") {
+        } else if (person.getRole() === "Engineer") {
             return `
             <div class="card" style="width: 18rem;">
             <div class="card-body">
-                <h4 class="card-title">${person.name}</h4>
-                <h6 class="card-text">${person.role}</h6>
+                <h4 class="card-title">${person.getName()}</h4>
+                <h6 class="card-text">${person.getRole()}</h6>
                 <ul class="list-group list-group-flush">
-                    <li class="list-group-item">ID: ${person.ID}</li>
-                    <li class="list-group-item">Email: ${person.email}</li>
-                    <li class="list-group-item">Github: <a href="github.com/${person.github}" class="card-link">${person.github}</a></li>
+                    <li class="list-group-item">ID: ${person.getID()}</li>
+                    <li class="list-group-item">Email: ${person.getEmail()}</li>
+                    <li class="list-group-item">Github: <a href="github.com/${person.getGithub()}" class="card-link">${person.getGithub()}</a></li>
                 </ul>
             </div>
         </div>
         `
-        } else if (person.role === "Engineer") {
+        } else if (person.getRole() === "Intern") {
             return `
             <div class="card" style="width: 18rem;">
             <div class="card-body">
-                <h4 class="card-title">${person.name}</h4>
-                <h6 class="card-text">${person.role}</h6>
+                <h4 class="card-title">${person.getName()}</h4>
+                <h6 class="card-text">${person.getRole()}</h6>
                 <ul class="list-group list-group-flush">
-                    <li class="list-group-item">ID: ${person.ID}</li>
-                    <li class="list-group-item">Email: ${person.email}</li>
-                    <li class="list-group-item">School: ${person.school} </li>
+                    <li class="list-group-item">ID: ${person.getID()}</li>
+                    <li class="list-group-item">Email: ${person.getEmail()}</li>
+                    <li class="list-group-item">School: ${person.getSchool()} </li>
                 </ul>
             </div>
         </div>
         `
         }
     })
+    let htmlOutput = "";
+    for (const card of teamCards) {
+        htmlOutput += card;
+    }
+    console.log(htmlOutput);
+    return htmlOutput;
 }
 
 // index.html file
@@ -175,11 +194,10 @@ return `
 
 
 // INITIALIZATION
-function init() {
-    managerPrompt();
-    employeePrompt();
-    generateHTML(createCard);
-    fs.writeFile('./dist/index.html', generateHTML(createCard), (err) =>
+async function init() {
+    await managerPrompt();
+    await employeePrompt();
+    fs.writeFile('./dist/index.html', generateHTML(createCard()), (err) =>
     err ? console.error(err) : console.log('You have successfully created your team!'));
 }
 
